6eb53154f377077162050f29266c57e7f27d887d
Edwin Miguel <edwintriana@gmail.com>
2013-11-25 16:21:33 -0500

    wiring interface

--- backbone/dbba_books/site/js/views/book.js
+++ backbone/dbba_books/site/js/views/book.js
@@ -5,8 +5,15 @@ app.BookView = Backbone.View.extend({
   tagName: 'div',
   className: 'bookContainer',
   template: _.template($('#bookTemplate').html()),
+  events:{
+    'click .delete': 'deleteBook'
+  },
   render: function(){
     this.$el.html(this.template(this.model.toJSON()));
     return this;
+  },
+  deleteBook : function(){
+    this.model.destroy();
+    this.remove();
   }
 });
--- backbone/dbba_books/site/js/views/library.js
+++ backbone/dbba_books/site/js/views/library.js
@@ -5,6 +5,7 @@ app.LibraryView = Backbone.View.extend({
   el: '#books',
   initialize: function(initialBooks){
     this.collection = new app.Library(initialBooks);
+    this.listenTo(this.collection, 'add', this.renderBook);
     this.render();
   },
   render: function(){
@@ -12,8 +13,23 @@ app.LibraryView = Backbone.View.extend({
       this.renderBook(item);
     }, this);
   },
+  events: {
+    'click #add': 'addBook'
+  },
   renderBook: function(item){
     var bookView = new app.BookView({model: item});
     this.$el.append(bookView.render().el);
+  },
+  addBook: function(e){
+    e.preventDefault();
+
+    var formData = {};
+
+    $('#addBook div').children('input').each(function(i, el){
+      if($(el).val() !=''){
+        formData[el.id] = $(el).val();
+      }
+    });
+    this.collection.add(new app.Book(formData));
   }
 });
