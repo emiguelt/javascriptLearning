97ea82c3e8051bf55da3290dfcefe00364cf1ba7
Edwin Miguel <edwintriana@gmail.com>
2015-02-04 21:57:05 -0500

    NodeJS, first app (in development)

---
+++ nodejs/app1/server.js
@@ -0,0 +1,21 @@
+var http = require('http');
+var url = require('url');
+
+function start(route){
+  function onRequest(req, rsp){
+    var pathname = url.parse(req.url).pathname;
+    console.log("Request for " + pathname + " received");
+
+    route(pathname);
+
+    rsp.writeHead(200, {"Content-Type": "text/plain"});
+    console.log("request received");
+    rsp.write("Hello node world!!!");
+    rsp.end();
+  };
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started");
+}
+
+exports.start = start;
